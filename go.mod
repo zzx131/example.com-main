90fc178819819af6a45056a0b3c954b5a9c6cb0c
zzx <zzx_nuc@163.com>
2022-04-04 12:58:01 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/main
+
+go 1.18
+
+require example.com/util v0.0.1
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/util"
+)
+
+func main() {
+	reselt := util.Add(1, 2)
+	fmt.Println(reselt)
+}
